14addecbdf27340e32b571aa89356aa7ebda24f8
James Lennon <jameslennon321@gmail.com>
2016-04-19 19:49:47 -0400

    Fixed printing and max_count

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import time
 if __name__ == '__main__':
     sleep_database.clean()
     start_time = time.time()
-    sleep_database.download_audio_files(save_audio=True, single_file=True, max_count=1)
+    sleep_database.download_audio_files(save_audio=True, single_file=True, max_count=None)
     print "TIME ELAPSED: " + str(time.time() - start_time)
 
